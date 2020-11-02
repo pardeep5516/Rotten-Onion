329a0734075843d9e8335e017b9d64fc1781903e
pardeep yadav <pardeepyadav2914@gmail.com>
2020-11-02 20:07:44 +0530

    User can see single reviews

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
+// const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
 
@@ -12,13 +13,15 @@ mongoose.connect("mongodb://localhost:27017/rotten-onion", {
 
 const Review = mongoose.model("Review", {
   title: String,
+  rating: Number,
   description: String,
   movieTitle: String,
 });
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   Review.find()
@@ -26,7 +29,7 @@ app.get("/", (req, res) => {
       res.render("reviews-index", { reviews: reviews });
     })
     .catch((err) => {
-      console.log("error...........");
+      console.log(err);
     });
 });
 
@@ -34,11 +37,21 @@ app.get("/reviews/new", (req, res) => {
   res.render("reviews-new", {});
 });
 
+app.get("/reviews/:id", (req, res) => {
+  Review.findById(req.params.id)
+    .then((review) => {
+      res.render("reviews-show", { review: review });
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
 app.post("/reviews", (req, res) => {
   Review.create(req.body)
     .then((review) => {
       console.log(review);
-      res.redirect("/");
+      res.redirect(`/reviews/${review._id}`);
     })
     .catch((err) => {
       console.log("error" + err.massage);
